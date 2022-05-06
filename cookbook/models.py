e0431596bd5054fe8e94dabe92a8c3a9ded8328e
Matje1979 <ibnruzd@yahoo.com>
2022-05-06 23:44:54 +0200

    Created models

--- cookbook/models.py
+++ cookbook/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.exceptions import ValidationError
 
 # Create your models here.
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(max_length=300, unique=True)
+    ingredients = models.ManyToManyField(Ingredient)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Rating(models.Model):
+    rate = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ["user", "recipe"]
+
+    def clean(self, *args, **kwargs):
+        if self.user == self.recipe.author:
+            raise ValidationError("User cannot rate its own recipe!")
+        super().clean(*args, **kwargs)
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Rating of {self.recipe} by {self.user}"
